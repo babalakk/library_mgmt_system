ea38e1df556fefaf063fb80af9a64690ae941611
beckhsu <beckhsu@synology.com>
2016-10-23 11:38:53 +0000

    install mysql utf8

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ mysql-install.yml
 
 ANSIBLE_REPO="git://github.com/ansible/ansible.git"
 ANSIBLE_WORKING_DIR="/opt/ansible"
+ANSIBLE_VARS=`ls ansible/vars`
 
 install_ansible() {
 	git clone $ANSIBLE_REPO $ANSIBLE_WORKING_DIR --recursive
@@ -18,16 +19,21 @@ install_ansible() {
 	cd -
 }
 
+setup_ansible() {
+	cd $ANSIBLE_WORKING_DIR
+	source ./hacking/env-setup
+	cd -
+}
+
 main() {
 
-	apt update && apt install python-dev python-setuptools libssl-dev -y
-	[ -d $ANSIBLE_WORKING_DIR ] && rm -rf $ANSIBLE_WORKING_DIR
-	install_ansible
+	apt update && apt install gcc python-dev python-setuptools libssl-dev -y
+	[ -d $ANSIBLE_WORKING_DIR ] && setup_ansible || install_ansible
 
 	ansible-galaxy install -r ansible/requirements.yml
 
 	for f in $PLAYBOOKS; do
-		ansible-playbook ansible/playbooks/${f} -e @ansible/vars/global.yml
+		ansible-playbook ansible/playbooks/${f} -e @${ANSIBLE_VARS}
 	done
 }
 
