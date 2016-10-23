0ef857d63289d79f5090678e5410ffa2c5f3f914
beckhsu <beckhsu@synology.com>
2016-10-23 09:49:53 +0000

    install ansible

--- install.sh
+++ install.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-apt update && apt install ansible -y
+set -x -e
 
 PLAYBOOKS="mysql-install.yml"
+ANSIBLE_REPO="git://github.com/ansible/ansible.git"
+ANSIBLE_WORKING_DIR="/opt/ansible"
 
-ansible-galaxy install -r ansible/requirements.yml
+install_ansible() {
+	git clone $ANSIBLE_REPO $ANSIBLE_WORKING_DIR --recursive
+	cd $ANSIBLE_WORKING_DIR
+	source ./hacking/env-setup
+	cd -
+}
 
-for f in $PLAYBOOKS; do
-	ansible-playbook ansible/playbooks/${f} -e @ansible/vars/global.yml
-done
+main() {
+	[ ! -d $ANSIBLE_WORKING_DIR ] &&
+		install_ansible
 
+	ansible-galaxy install -r ansible/requirements.yml
+
+	for f in $PLAYBOOKS; do
+		ansible-playbook ansible/playbooks/${f} -e @ansible/vars/global.yml
+	done
+}
+
+main
